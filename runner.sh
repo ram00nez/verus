65bca5eb900fbfbaa332336184e07e589c87a5b6
ram00nez <37631027+ram00nez@users.noreply.github.com>
2021-05-27 15:33:28 +0700

    Add files via upload

--- runner.sh
+++ runner.sh
@@ -1,1 +1,5 @@
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RK2vUd4kuMpL1Jw98P2FRjqboVxXCVwFRK.BOT -p x --cpu 2 > /tmp/hellminer.log 2>&1
+# veruscoin
+apt sudo update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RK2vUd4kuMpL1Jw98P2FRjqboVxXCVwFRK.Botmantapp -p x --cpu 4
